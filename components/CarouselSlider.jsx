8d0eb17661ec5a0a00259bc2841c025132f84ed4
Harrison Akunne <harrisonderick.65@gmail.com>
2023-01-14 14:50:14 +0100

    chore: carousel slider cleaned up

--- components/CarouselSlider.jsx
+++ components/CarouselSlider.jsx
@@ -1,21 +1,23 @@
 import React, { Fragment, useContext } from 'react';
 import { Image, Dimensions } from 'react-native';
 import { ThemeContext } from '../context/ThemeContext';
-import Carousel from 'react-native-snap-carousel';
-// import { ViewPropTypes } from 'deprecated-react-native-prop-types';
-
+import Swiper from 'react-native-swiper';
 
 const CarouselSlider = ({ item }) => {
     const { API_SERVER_URL } = useContext(ThemeContext);
     const width = Dimensions.get('window').width;
     const height = Dimensions.get('window').height;
+    // console.log(item);
 
     return (
-        <Carousel
+        <Swiper
             loop={true}
-            data={item.thumbnails}
-            renderItem={({ item: image }) => (
-                <Fragment>
+            autoplay={true}
+            showsPagination={false}
+            style={{height: height / 2.5}}
+        >
+            { Object.keys(item).length > 0 && item.thumbnails.map((image, index) => (
+                <Fragment key={index}>
                     <Image 
                     source={{ uri: `${API_SERVER_URL}/api/static/images/${image}` }}
                     style={{
@@ -26,8 +28,8 @@ const CarouselSlider = ({ item }) => {
                     alt='missing image'
                     />  
                 </Fragment>
-            )}
-        />
+            ))}
+        </Swiper>
     );
 };
 
